50a55d350f664951414b35a002734b44c8129f48
Felipse <samborskaya.r@yandex.ru>
2024-12-28 11:40:47 +0300

    fixed naming

--- src/pages/newPage/NewPageComponent.vue
+++ src/pages/newPage/NewPageComponent.vue
@@ -1,19 +1,4 @@
 <template>
-  <header class="header">
-    <div class="header_container">
-      <div class="header_logo">
-        <img src="../../../static/img/met-images/logo.svg" alt="logo" />
-      </div>
-      <nav class="header_menu">
-        <a href="#" class="header_menu-link">Фулфилмент</a>
-        <a href="#" class="header_menu-link">О нас</a>
-        <a href="#" class="header_menu-link">Преимущества</a>
-        <a href="#" class="header_menu-link">FAQ</a>
-        <button type="button" class="header_menu-button orange-button">
-          КОНСУЛЬТАЦИЯ
-        </button>
-      </nav>
-    </div>
-  </header>
+  <Header />
 </template>
 <style scoped></style>
