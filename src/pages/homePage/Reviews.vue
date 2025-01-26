afb5055c30ad0a1a1c9ab6fabf70417f7ee33caa
Portnyagin Andrei <portnyagin.a@rocketsales.ru>
2025-01-26 19:16:33 +0300

    Fix first Slide

--- src/pages/homePage/Reviews.vue
+++ src/pages/homePage/Reviews.vue
@@ -2,54 +2,57 @@
   <section class="reviews" id="reviews">
     <h3 class="reviews_title">отзывы и кейсы</h3>
     <div class="reviews_slider-wrapper">
-      <div
-        class="reviews_slider-left-arrow-wrapper reviews_slider-arrows-wrapper_mob"
-      >
-        <svg
-          class="reviews_slider_left-arrow"
-          width="36"
-          height="36"
-          viewBox="0 0 36 36"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M7.5 18H28.5"
-            stroke="inherit"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M15 25.5L7.5 18"
-            stroke="inherit"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M15 10.5L7.5 18"
-            stroke="inherit"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-      </div>
       <div class="reviews_slider">
+        <button class="reviews_slider-arrow-wrapper reviews_slider-left-arrow">
+          <svg
+            class="reviews_slider_left-arrow"
+            width="36"
+            height="36"
+            viewBox="0 0 36 36"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M7.5 18H28.5"
+              stroke="inherit"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M15 25.5L7.5 18"
+              stroke="inherit"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M15 10.5L7.5 18"
+              stroke="inherit"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </button>
         <swiper
           :observer="true"
           :observeParents="true"
           slidesPerView="auto"
           :spaceBetween="30"
           :speed="500"
+          :modules="[Navigation]"
           :breakpoints="{
             1024: {
               spaceBetween: 10,
             },
-            1250: {
-              spaceBetween: 30,
+            1220: {
+              spaceBetween: 20,
             },
+          }"
+          :navigation="{
+            prevEl: '.reviews_slider-left-arrow',
+            nextEl: '.reviews_slider-right-arrow',
           }"
         >
           <swiper-slide>
@@ -106,24 +109,6 @@
               </p>
             </div>
           </swiper-slide>
-          <swiper-slide>
-            <div class="reviews_slide">
-              <div class="reviews_slide-header">
-                <img
-                  class="reviews_slide-photo"
-                  src="../../../static/img/met-images/Olga.png"
-                  alt="user-icon"
-                />
-                <p class="reviews_slide-title">Ольга Заворотная</p>
-              </div>
-              <p class="reviews_slide-description">
-                Компания сумела качественно оказать не только услугу по
-                продвижению, но и взяла на себя ряд сопутствующих услуг, такие
-                как оформление и оптимизация карточек, а также настройка и
-                запуск рекламы на маркетплейсах.
-              </p>
-            </div>
-          </swiper-slide>
           <swiper-slide
             ><div class="reviews_slide">
               <div class="reviews_slide-header">
@@ -160,113 +145,40 @@
               </p>
             </div>
           </swiper-slide>
-          <swiper-slide>
-            <div class="reviews_slide">
-              <div class="reviews_slide-header">
-                <img
-                  class="reviews_slide-photo"
-                  src="../../../static/img/met-images/Ekaterina.png"
-                  alt="user-icon"
-                />
-                <p class="reviews_slide-title">Екатерина Кривцун</p>
-              </div>
-              <p class="reviews_slide-description">
-                Компания сумела качественно оказать не только услугу по
-                продвижению, но и взяла на себя ряд сопутствующих услуг, такие
-                как оформление и оптимизация карточек, а также настройка и
-                запуск рекламы на маркетплейсах.
-              </p>
-            </div>
-          </swiper-slide>
-          <swiper-slide>
-            <div class="reviews_slide">
-              <div class="reviews_slide-header">
-                <img
-                  class="reviews_slide-photo"
-                  src="../../../static/img/met-images/Olga.png"
-                  alt="user-icon"
-                />
-                <p class="reviews_slide-title">Ольга Заворотная</p>
-              </div>
-              <p class="reviews_slide-description">
-                Компания сумела качественно оказать не только услугу по
-                продвижению, но и взяла на себя ряд сопутствующих услуг, такие
-                как оформление и оптимизация карточек, а также настройка и
-                запуск рекламы на маркетплейсах.
-              </p>
-            </div>
-          </swiper-slide>
-          <swiper-slide>
-            <div class="reviews_slide">
-              <div class="reviews_slide-header">
-                <img
-                  class="reviews_slide-photo"
-                  src="../../../static/img/met-images/Olga.png"
-                  alt="user-icon"
-                />
-                <p class="reviews_slide-title">Ольга Заворотная</p>
-              </div>
-              <p class="reviews_slide-description">
-                Компания сумела качественно оказать не только услугу по
-                продвижению, но и взяла на себя ряд сопутствующих услуг, такие
-                как оформление и оптимизация карточек, а также настройка и
-                запуск рекламы на маркетплейсах.
-              </p>
-            </div>
-          </swiper-slide>
-          <swiper-slide
-            ><div class="reviews_slide">
-              <div class="reviews_slide-header">
-                <img
-                  class="reviews_slide-photo"
-                  src="../../../static/img/met-images/Olga.png"
-                  alt="user-icon"
-                />
-                <p class="reviews_slide-title">Владимир Высоцкий</p>
-              </div>
-              <p class="reviews_slide-description">
-                Компания сумела качественно оказать не только услугу по
-                продвижению, но и взяла на себя ряд сопутствующих услуг, такие
-                как оформление и оптимизация карточек, а также настройка и
-                запуск рекламы на маркетплейсах.
-              </p>
-            </div>
-          </swiper-slide>
         </swiper>
-      </div>
-      <div
-        class="reviews_slider-right-arrow-wrapper reviews_slider-arrows-wrapper_mob"
-      >
-        <svg
-          class="reviews_slider_right-arrow active-arrow"
-          width="36"
-          height="36"
-          viewBox="0 0 36 36"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M28.5 18H7.5"
-            stroke="inherit"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M21 25.5L28.5 18"
-            stroke="inherit"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M21 10.5L28.5 18"
-            stroke="inherit"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
+
+        <button class="reviews_slider-arrow-wrapper reviews_slider-right-arrow">
+          <svg
+            class="reviews_slider_right-arrow active-arrow"
+            width="36"
+            height="36"
+            viewBox="0 0 36 36"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M28.5 18H7.5"
+              stroke="inherit"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M21 25.5L28.5 18"
+              stroke="inherit"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M21 10.5L28.5 18"
+              stroke="inherit"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </button>
       </div>
     </div>
     <button class="reviews_button orange-button" type="button">
@@ -276,11 +188,9 @@
 </template>
 <script setup lang="ts">
 import { Swiper, SwiperSlide } from "swiper/vue";
+import { Navigation } from "swiper/modules";
 </script>
-<style scoped>
-.swiper {
-  width: 82%;
-}
+<style scoped lang="scss">
 .swiper-slide {
   width: auto;
 }
@@ -320,12 +230,17 @@ import { Swiper, SwiperSlide } from "swiper/vue";
   gap: 14px;
   /* width: 100%; */
 }
+.swiper {
+  width: 100%;
+  max-width: 1200px;
+}
 .reviews_slider {
+  position: relative;
   display: flex;
   flex-direction: row;
-  overflow: hidden;
   gap: 20px;
-  /* width: 1200px; */
+  max-width: 1320px;
+  padding: 0 60px;
   width: 100%;
 }
 .reviews_slide {
@@ -371,7 +286,8 @@ import { Swiper, SwiperSlide } from "swiper/vue";
   align-items: center;
   position: absolute;
 } */
-.reviews_slider-left-arrow-wrapper {
+
+.reviews_slider-arrow-wrapper {
   border: 1px solid #cbcbcb;
   width: 36px;
   height: 36px;
@@ -381,47 +297,43 @@ import { Swiper, SwiperSlide } from "swiper/vue";
   align-items: center;
   position: absolute;
   top: 50%;
-  left: 70px;
-}
-.reviews_slider-right-arrow-wrapper {
-  border: 1px solid #cbcbcb;
-  width: 36px;
-  height: 36px;
-  border-radius: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: absolute;
-  top: 50%;
-  right: 70px;
-}
-.reviews_slider-left-arrow-wrapper:hover {
+  background-color: transparent;
+  padding: 0;
   cursor: pointer;
-  opacity: 0.7;
+
+  & :hover {
+    opacity: 0.7;
+  }
 }
-.reviews_slider-right-arrow-wrapper:hover {
-  cursor: pointer;
-  opacity: 0.7;
+.reviews_slider-left-arrow {
+  left: 10px;
+}
+.reviews_slider-right-arrow {
+  right: 10px;
 }
 .reviews_slider_left-arrow {
-  stroke: #cbcbcb;
-}
-.reviews_slider_right-arrow {
-  stroke: #cbcbcb;
-}
-.active-arrow {
   stroke: #181818;
 }
+.reviews_slider_right-arrow {
+  stroke: #181818;
+}
+
+.swiper-button-disabled > .reviews_slider_left-arrow,
+.swiper-button-disabled > .reviews_slider_right-arrow {
+  stroke: #cbcbcb;
+}
+
 @media (min-width: 681px) and (max-width: 1220px) {
-  .reviews_slider-right-arrow-wrapper {
+  .reviews_slider-arrow-wrapper {
     display: none;
   }
-  .reviews_slider-left-arrow-wrapper {
-    display: none;
+
+  .reviews_slider {
+    padding: 0 16px;
   }
 }
 @media (min-width: 320px) and (max-width: 680px) {
-  .reviews_slider-arrows-wrapper_mob {
+  .reviews_slider-arrow-wrapper {
     display: none;
   }
   .reviews {
@@ -459,6 +371,7 @@ import { Swiper, SwiperSlide } from "swiper/vue";
   }
   .reviews_slider {
     gap: 14px;
+    padding: 0;
   }
   .reviews_slide-title {
     font-size: 17px;
